d26c1f2b5cb55447d581c0028e3761d763ccbc09
NITIN JAIN <official.nitinjain@gmail.com>
2020-01-06 14:46:52 +0800

    terraform initial working script

---
+++ connections.tf
@@ -0,0 +1,7 @@
+# you need to provide your respective subscription values for your account.
+provider  "azurerm" {
+   subscription_id = "*******************************"
+   client_id = "*******************************"
+   client_secret = "*******************************"
+   tenant_id = "*******************************"
+}
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  default = "resources"
+}
+
+variable "prefix" {
+  default = "tfvmex"
+}
+
+variable "location" {
+  default = "West US 2"
+}
+
+variable "size" {
+  default = "1024M"
+}
+
+
